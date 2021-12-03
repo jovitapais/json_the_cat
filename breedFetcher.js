7ad47dd382fc2d1e03e6cc538c59bacec33a24ec
Jovita <jovitaspais@gmail.com>
2021-12-03 01:57:42 +0000

    adding files to github

---
+++ breedFetcher.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const args = process.argv.splice(2);
+const breed = args[0];
+console.log(breed);
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
+ // console.error('error:', error); // Print the error if one occurred
+ // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+ // console.log('body:', body); // Print the HTML for the Google homepage.
+  //console.log(typeof body);
+  const data = JSON.parse(body);
+  //console.log(data);
+  //console.log(typeof data);
+  console.log(data[0].description);
+  
+});
+
+
+
+
