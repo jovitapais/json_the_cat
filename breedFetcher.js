8bb29ca3c7126f71bfc6a1b6243bb271bf7c65eb
Jovita <jovitaspais@gmail.com>
2021-12-03 02:35:52 +0000

    Handle request errors and print the error details to the screen.

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,10 +3,13 @@ const args = process.argv.splice(2);
 const breed = args[0];
 console.log(breed);
 request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
- // console.error('error:', error); // Print the error if one occurred
- // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
- // console.log('body:', body); // Print the HTML for the Google homepage.
+  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
   //console.log(typeof body);
+  if(error) {
+    console.error('error:', error); // Print the error if one occurred
+    return;
+  } 
   const data = JSON.parse(body);
   //console.log(data);
   //console.log(typeof data);
